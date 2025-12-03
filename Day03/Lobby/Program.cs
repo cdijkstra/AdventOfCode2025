c6038f9151690a20ad8e617fdb4e54d7647c7ccd
Casper <c.d.dijkstra@hva.nl>
2025-12-03 07:27:31 +0100

    Refactor part 2

--- Day03/Lobby/Program.cs
+++ Day03/Lobby/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
+namespace Lobby;
 
-class Program
+internal abstract class Program
 {
     static void Main(string[] args)
     {
@@ -8,7 +9,7 @@ class Program
         var banks = ReadData("data.txt");
         Debug.Assert(CalculateDoubleJolt(testBanks) == 357);
         Console.WriteLine(CalculateDoubleJolt(banks));
-        // Debug.Assert(CalculateMultipleJolt(testBanks) == 3121910778619);
+        Debug.Assert(CalculateMultipleJolt(testBanks) == 3121910778619);
         Console.WriteLine(CalculateMultipleJolt(banks));
     }
 
@@ -48,22 +49,10 @@ class Program
         foreach (var bank in banks)
         {
             var jolt = string.Empty;
-            var index = 0;
+            var index = -1;
             var elementsLeft = elements;
-            Console.WriteLine(bank);
-            
-            var firstJolt = bank.Substring(index, bank.Length - elements)
-                .Select((c, i) => new { Digit = int.Parse(c.ToString()), Index = i })
-                .OrderByDescending(x => x.Digit)
-                .ThenBy(x => x.Index)
-                .First(); // Choose first match
 
-            Console.WriteLine("First : " + firstJolt.Digit + " at " + firstJolt.Index);
-            jolt += firstJolt.Digit;
-            elementsLeft--;
-            index = firstJolt.Index;
-
-            foreach (var elementLeft in Enumerable.Range(0, elements - 1))
+            foreach (var _ in Enumerable.Range(0, elements))
             {
                 var newJolt = bank[(index + 1)..^(elementsLeft - 1)]
                     .Select((c, i) => new { Digit = int.Parse(c.ToString()), Index = i })
@@ -71,13 +60,11 @@ class Program
                     .ThenBy(x => x.Index)
                     .First();
                 
-                Console.WriteLine($"Considering {bank[(index + 1)..^(elementsLeft - 1)]} (left out {elementsLeft - 1} elements) ===> {newJolt.Digit} at {newJolt.Index}");
                 elementsLeft--;
                 index += newJolt.Index + 1;
                 jolt += newJolt.Digit;
             }
             
-            Console.WriteLine("Jolt = " + jolt);
             joltage += long.Parse(jolt);
         }
         return joltage;
