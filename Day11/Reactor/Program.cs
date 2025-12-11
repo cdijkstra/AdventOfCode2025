e0aadc252b1ff67bdd6190befce206074ecf9fd2
Casper <c.d.dijkstra@hva.nl>
2025-12-11 09:09:40 +0100

    About to refactor for part 2

--- Day11/Reactor/Program.cs
+++ Day11/Reactor/Program.cs
@@ -14,6 +14,8 @@ public class Program
     {
         Debug.Assert(Part1("testdata.txt") == 5);
         Console.WriteLine(Part1("data.txt"));
+        Debug.Assert(Part2("testdata2.txt") == 2);
+        Console.WriteLine(Part2("data.txt"));
     }
 
     private static void ReadData(string fileName)
@@ -35,12 +37,20 @@ public class Program
         return routes;
         
     }
+    private static int Part2(string fileName)
+    {
+        ReadData(fileName);
+        var node = _nodes.Single(n => n.Name == "svr");
+        var routes = TraverseDacFft(node, new());
+        Console.WriteLine(routes);
+        return routes;
+        
+    }
 
     private static int Traverse(Node node, List<string> visited)
     {
         var finished = 0;
         var validNeighbors = node.ConnectedTo.Where(n => !visited.Contains(n)).ToList();
-        Console.WriteLine($"{node.Name} -> {string.Join(',', visited)}; valid neighbors: {string.Join(',', validNeighbors)}");
         
         foreach (var neighbor in validNeighbors)
         {
@@ -56,5 +66,23 @@ public class Program
         return finished;
     }
     
-    
+    private static int TraverseDacFft(Node node, List<string> visited)
+    {
+        var finished = 0;
+        var validNeighbors = node.ConnectedTo.Where(n => !visited.Contains(n)).ToList();
+        
+        foreach (var neighbor in validNeighbors)
+        {
+            // Console.WriteLine($"{node.Name} -> {string.Join(',', visited)}; valid neighbors: {string.Join(',', validNeighbors)}");
+            if (neighbor == "out")
+            {
+                return visited.Contains("fft") && visited.Contains("dac") ? 1 : 0;
+            }
+            
+            var next = _nodes.Single(n => n.Name == neighbor);
+            finished += TraverseDacFft(next, visited.Append(neighbor).ToList());
+        }
+
+        return finished;
+    }
 }
